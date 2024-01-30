1a79c241abcfc6b279a1c5773622e0bb132bcb44
Yohwan Lim <dyghks7102@naver.com>
2024-01-30 22:29:01 +0900

    feat: test

--- src/extends1/overriding/CarMain.java
+++ src/extends1/overriding/CarMain.java
@@ -5,11 +5,18 @@ public class CarMain {
     public static void main(String[] args) {
         ElectricCar electricCar = new ElectricCar();
         electricCar.move();
+        electricCar.charge();
+
+        Car electricCar2 = new ElectricCar();
+        electricCar2.move(); // == electricCar.move()
+//        electricCar2.charge(); // Car 클래스에 charge 메서드가 존재하지 않아 사용 불가능
 
         GasCar gasCar = new GasCar();
         gasCar.move();
 
         HydrogenCar hydrogenCar = new HydrogenCar();
         hydrogenCar.move();
+        hydrogenCar.fillHydrogen();
+
     }
 }
